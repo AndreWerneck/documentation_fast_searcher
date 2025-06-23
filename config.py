97d85702f6fb81dfaa783fd1383466173bdf6a05
AndreWerneck <andrecw97@gmail.com>
2025-06-23 17:06:55 +0200

    add scripts for installing and config everything related to the llm that will be used for generating the response

---
+++ config.py
@@ -0,0 +1,8 @@
+MODEL_PATH = "models/mistral-7b-instruct-v0.2.Q4_K_M.gguf"
+N_CTX = 4096
+MAX_TOKENS = 512
+TEMPERATURE = 0.1
+TOP_P = 0.95
+TOP_K = 40
+REPEPETITION_PENALTY = 1.1
+STOP = ["</s>", "###", "User:", "System:"]
